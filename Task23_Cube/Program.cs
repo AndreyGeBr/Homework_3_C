ce2760a94244113bbdd3c8659f219055fef668ca
Andrey <a89615076005@yandex.ru>
2022-09-25 20:25:27 +0300

    add task 23

---
+++ Task23_Cube/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+int count = 1;
+while (count <= n)
+{
+    Console.Write($"{Math.Pow(count, 3)}");
+    if (count != n)
+        Console.Write(", ");
+    count++;
+}
